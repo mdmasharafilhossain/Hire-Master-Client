d3a121b4e45a98f2fc41783f5e96265d0407a1a6
Miran Ibrahim <moeenuddin.miran@gmail.com>
2024-01-16 20:32:52 +0600

    necessary routes created

---
+++ src/LayOut/MainLayOut.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Comonents/Navbar/Navbar";
+import Footer from "../Comonents/Footer/Footer";
+
+
+const MainLayOut = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default MainLayOut;
