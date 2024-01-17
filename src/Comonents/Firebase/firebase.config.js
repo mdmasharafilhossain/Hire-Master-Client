d7e43973b6377e36ffd80ad1aabea43361051fd6
Mohosin212601~ <mohosin2126@gmail.com>
2024-01-17 20:31:16 +0800

    created signup form and set it's route

--- src/Comonents/Firebase/firebase.config.js
+++ src/Comonents/Firebase/firebase.config.js
@@ -5,6 +5,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_apiKey,
     authDomain: import.meta.env.VITE_authDomain,
