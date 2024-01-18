eca8cdaabd40aa57fdc96ab6c9ba56155f66be4d
Miran Ibrahim <moeenuddin.miran@gmail.com>
2024-01-19 00:14:36 +0600

    [BugFix](UI color change): Element color change according to UI theme

--- src/Comonents/JobList/JobList.jsx
+++ src/Comonents/JobList/JobList.jsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Tab,
   TabIndicator,
   TabList,
@@ -14,8 +13,9 @@ const JobList = () => {
   return (
     <div className="flex flex-col items-center ">
       <h1 className="text-5xl font-bold">Job Listing</h1>
+      {/* --------------------Tab start from here------------------ */}
       <div className="mt-8 w-full md:w-[70%] ">
-        <Tabs  align="center" variant="unstyled">
+        <Tabs align="center" variant="unstyled">
           <TabList>
             <Tab>Featured</Tab>
             <Tab>Full Time</Tab>
@@ -25,11 +25,12 @@ const JobList = () => {
           <TabIndicator
             mt="-1.5px"
             height="2px"
-            bg="blue.500"
+            bg="orange.400"
             borderRadius="1px"
           />
-          <TabPanels >
-            <TabPanel >
+          <TabPanels>
+            <TabPanel>
+              {/* -------------Individual Job Card----------------- */}
               <SingleJobList
                 companyLogo={companyLogo1}
                 jobName={"Software Engineer"}
@@ -37,10 +38,10 @@ const JobList = () => {
                 time={"Full Time"}
                 salary={"$95,000 - $120,000"}
                 deadline={"15 Feb 2024"}
-                
               />
             </TabPanel>
             <TabPanel>
+              {/* -------------Individual Job Card----------------- */}
               <SingleJobList
                 companyLogo={companyLogo1}
                 jobName={"Backend Engineer"}
@@ -51,6 +52,7 @@ const JobList = () => {
               />
             </TabPanel>
             <TabPanel>
+              {/* -------------Individual Job Card----------------- */}
               <SingleJobList
                 companyLogo={companyLogo1}
                 jobName={"Technical SEO"}
@@ -61,6 +63,7 @@ const JobList = () => {
               />
             </TabPanel>
             <TabPanel>
+              {/* -------------Individual Job Card----------------- */}
               <SingleJobList
                 companyLogo={companyLogo1}
                 jobName={"Web Developer Intern"}
@@ -74,14 +77,13 @@ const JobList = () => {
         </Tabs>
       </div>
 
-      <div>
-        <Button colorScheme="green" size="lg">
+      <div className="w-full flex justify-center">
+        <button className="btn bg-[#FF3811] text-white w-[20%] mx-auto">
           Brows More Jobs
-        </Button>
+        </button>
       </div>
     </div>
   );
 };
 
 export default JobList;
-
