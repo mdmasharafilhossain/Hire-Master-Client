c433bfe90eaeed0035f9049779256bfec17341a9
M-Arafat61 <arafjust16@gmail.com>
2024-01-19 12:42:34 +0600

    [feat]\(testimonials-footer\): responsive footer and testimonials

--- src/Comonents/Footer/Footer.jsx
+++ src/Comonents/Footer/Footer.jsx
@@ -4,43 +4,86 @@ import {
   FaGooglePlus,
 } from "react-icons/fa";
 import { FaSquareTwitter, FaInstagram } from "react-icons/fa6";
+import logo from "../../assets/hire_master-logo.png";
+import { FaCaretRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='mt-16 mx-auto'>
-      <footer className='grid grid-cols-1 lg:grid-cols-4 footer py-10 '>
-        <nav className=''>
-          <img src='' alt='logo' />
-          <header className=' text-xl'>What HireMaster Does?</header>
-          <p>
-            HireMaster is an innovative web application that serves as a
-            comprehensive platform for agencies, job applicants, and recruiters
-          </p>
-        </nav>
-        <nav>
-          <header className='font-semibold text-lg'>
-            Candidate Opportunities
-          </header>
-          <a className='link link-hover'>Add A Resume</a>
-          <a className='link link-hover'> Job Alerts</a>
-          <a className='link link-hover'> My Account</a>
-        </nav>
-        <nav>
-          <header className='font-semibold text-lg'>
-            Employers Opportunities
-          </header>
-          <a className='link link-hover'> Browse Candidates</a>
-          <a className='link link-hover'> Browse Candidates</a>
-          <a className='link link-hover'> Browse Candidates</a>
-        </nav>
-        <nav>
-          <header className='font-semibold text-lg'>Join the Community</header>
-          <a className='link link-hover'>community</a>
-          <a className='link link-hover'>community</a>
-          <a className='link link-hover'>community</a>
-        </nav>
+    <div className='container mx-auto px-3 mt-20 mb-10'>
+      <hr />
+      <footer className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 py-10 place-items-start'>
+        <div className='md:col-span-2 lg:col-span-1'>
+          <nav className='text-start mb-4'>
+            <img className='w-28 mx-auto' src={logo} alt='logo' />
+          </nav>
+        </div>
+        <div className='md:col-span-1 space-y-2'>
+          <header className='font-semibold text-lg'>Job Seekers</header>
+          <div className=''>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Create Account
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              List of Features
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Video CV
+              <span className='p-[4px] border border-[#ff3811] rounded-2xl ml-1'>
+                new
+              </span>
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              FAQ
+            </Link>
+          </div>
+        </div>
+
+        <div className='md:col-span-1 space-y-2'>
+          <header className='font-semibold text-lg'>Employers</header>
+          <div>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Create Account
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Products/Service
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Post a Job
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              FAQ
+            </Link>
+          </div>
+        </div>
+        <div className='md:col-span-1 space-y-2'>
+          <header className='font-semibold text-lg'>Community</header>
+          <div>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Events
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Community Hub
+            </Link>
+            <Link className='flex items-center'>
+              <FaCaretRight />
+              Community Standards
+            </Link>
+          </div>
+        </div>
       </footer>
-      <footer className='footer px-10 py-4 border-t text-base-content border-base-300'>
+
+      <footer className='footer py-4 border-t text-base-content border-base-300'>
         <aside className='items-center grid-flow-col'>
           <p>&copy; 2024 Hire Master. All Rights Reserved.</p>
         </aside>
