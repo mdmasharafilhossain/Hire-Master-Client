cea07e814d64603f94d7f19a6fec51b5cfa95ea5
M-Arafat61 <arafjust16@gmail.com>
2024-01-23 16:31:53 +0600

    [feat](register_login):basic designe and responsiveness

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,135 +1,117 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { FcGoogle } from 'react-icons/fc'
-import { useContext } from 'react';
-import swal from 'sweetalert';
-import { AuthContext } from '../../Comonents/AuthProvider/AuthProvider';
-
-
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { useContext } from "react";
+import swal from "sweetalert";
+import { AuthContext } from "../../Comonents/AuthProvider/AuthProvider";
 
 const Login = () => {
-  const {signIn , googleSignIn } = useContext(AuthContext);
+  const { signIn, googleSignIn } = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
-  
- // google sign in 
- const handleGoogleSignIn = () =>{
-    googleSignIn()
-    .then(result =>{
+
+  // google sign in
+  const handleGoogleSignIn = () => {
+    googleSignIn().then(result => {
+      console.log(result);
       navigate(from, { replace: true });
-       
-    })
-}
+    });
+  };
+  const handleLogin = e => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
 
-// email sign up 
-const handleLogin=e=>{
-e.preventDefault()
-const form=e.target 
-const email=form.email.value 
-const password =form.password.value 
-
-
-
-
-signIn(email, password)
-.then(result => {
-   console.log(result)
-navigate(location?.state? location.state:"/")
-return (swal("Success!", "Login Successful", "success"))
-})
-.catch(error => console.log(error))
-return (swal("Error!", "Already User", "error"))
-
-}
-
-
-
-
+    signIn(email, password)
+      .then(result => {
+        if (result) {
+          navigate(location?.state ? location.state : "/");
+          return swal("Success!", "Login Successful", "success");
+        }
+      })
+      .catch(error => console.log(error));
+    return swal("Error!", "Already User", "error");
+  };
 
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
-        <div className='mb-8 text-center'>
-          <h1 className='my-3 text-4xl font-bold'>Log In</h1>
-          <p className='text-sm text-gray-400'>
-            Sign in to access your account
-          </p>
-        </div>
-        <form
-        onSubmit={handleLogin}
-        >
-          <div className='space-y-4'>
-            <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
-                Email address
-              </label>
-              <input
-                type='email'
-                name='email'
-                id='email'
-                required
-                placeholder='Enter Your Email Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
-              />
+    <div className='p-2 container mx-auto flex flex-col lg:flex-row space-x-10 justify-between items-center my-20'>
+      <div className='w-full mx-auto'>
+        <img
+          className=''
+          src='https://i.ibb.co/VCWMv70/10173919-8619.jpg'
+          alt=''
+        />
+      </div>
+      <div className='w-full mx-auto lg:w-3/4 border rounded-2xl overflow-hidden px-10 md:px-16 lg:px-24 py-10 md:py-14 lg:py-20'>
+        <div className='card '>
+          <div className='flex flex-col w-full'>
+            <div className='text-center'>
+              <h1 className='text-2xl md:text-3xl font-semibold mb-5'>
+                Log in to HireMaster{" "}
+              </h1>
             </div>
             <div>
-              <div className='flex justify-between'>
-                <label htmlFor='password' className='text-sm mb-2'>
-                  Password
-                </label>
+              <form className='space-y-4' onSubmit={handleLogin}>
+                <div className='form-control'>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Your Email'
+                    className='border border-gray-300 px-3 py-2 outline-none'
+                    required
+                  />
+                </div>
+                <div className='form-control'>
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    className='border border-gray-300 px-3 py-2 outline-none'
+                    required
+                  />
+                </div>
+                <div className='form-control'>
+                  <button
+                    type='submit'
+                    className='group relative overflow-hidden py-2 rounded-2xl bg-[#FF3811] font-semibold text-white'
+                  >
+                    Login
+                    <div className='absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30'></div>
+                  </button>
+                </div>
+                <div className='flex justify-center items-center'>
+                  <span className='w-full border border-[#FF3811]'></span>
+                  <span className='px-4'>Or</span>
+                  <span className='w-full border border-[#FF3811]'></span>
+                </div>
+                <button
+                  onClick={handleGoogleSignIn}
+                  className='btn btn-outline btn-warning w-full rounded-md overflow-hidden text-xs sm:text-xl  font-bold'
+                >
+                  <FcGoogle className='text-xl' /> Continue with Google
+                </button>
+              </form>
+              <div className='form-control mt-16 space-y-5'>
+                <div className='flex justify-between items-center'>
+                  <span className='w-full border border-[#FF3811]'></span>
+                  <span className='text-xs sm:text-base w-full text-nowrap px-2'>
+                    Do not have an account?
+                  </span>
+                  <span className='w-full border border-[#FF3811]'></span>
+                </div>
+                <Link to='/signup' className='w-full mx-auto'>
+                  <button className='flex font-semibold mx-auto justify-center w-full sm:w-1/2 border px-5 py-2 rounded-full border-yellow-300'>
+                    Sign up
+                  </button>
+                </Link>
               </div>
-              <input
-                type='password'
-                name='password'
-                autoComplete='current-password'
-                id='password'
-                required
-                placeholder='*******'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-              />
             </div>
           </div>
-
-          <div>
-            <button
-              type='submit'
-              className='bg-rose-500 w-full rounded-md py-3 text-white'
-            >
-              Continue
-            </button>
-          </div>
-        </form>
-        <div className='space-y-1'>
-          <button className='text-xs hover:underline hover:text-rose-500 text-gray-400'>
-            Forgot password?
-          </button>
         </div>
-        <div className='flex items-center pt-4 space-x-1'>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-          <p className='px-3 text-sm dark:text-gray-400'>
-            Login with social accounts
-          </p>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-        </div>
-        <div onClick={handleGoogleSignIn} className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
-          <FcGoogle size={32} />
-
-          <p>Continue with Google</p>
-        </div>
-        <p className='px-6 text-sm text-center text-gray-400'>
-          Don&apos;t have an account yet?{' '}
-          <Link
-            to='/signup'
-            className='hover:underline hover:text-rose-500 text-gray-600'
-          >
-            Sign up
-          </Link>
-          .
-        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
