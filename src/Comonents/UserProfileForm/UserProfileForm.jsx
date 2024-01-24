9829eb58abaa036644ec138d149e2238513fe33b
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-01-24 12:02:49 +0600

    imgbb added for hosting

--- src/Comonents/UserProfileForm/UserProfileForm.jsx
+++ src/Comonents/UserProfileForm/UserProfileForm.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from './../AuthProvider/AuthProvider';
+import { useForm } from "react-hook-form";
+
+
+const Image_Hosting_key=import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const Profile_Hosting=`https://api.imgbb.com/1/upload?key=${Image_Hosting_key}`;
 
 
 const UserProfileForm = () => {
+    const {user} = useContext(AuthContext)
+    const { register, handleSubmit } = useForm();
     return (
         <div>
            <h2>User Profile</h2> 
