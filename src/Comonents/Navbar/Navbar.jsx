e296ff1fb85183e6fd0d214989336f43ae1b8b85
M-Arafat61 <arafjust16@gmail.com>
2024-01-24 12:42:40 +0600

    [feature](jobs):jobs page initialization

--- src/Comonents/Navbar/Navbar.jsx
+++ src/Comonents/Navbar/Navbar.jsx
@@ -17,67 +17,65 @@ const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
-    <div className='container mx-auto'>
-      <div className='navbar absolute mt-[450px] bg-base-100 h-[10px]'>
-        <button
-          ref={btnRef}
-          display={{ base: "block", md: "none" }}
-          onClick={onOpen}
-          size={"sm"}
-          //   background={"none"}
-        >
-          <MdOutlineDashboard className='text-[#FF3811] w-full' size={38} />
-        </button>
-        <Drawer
-          isOpen={isOpen}
-          placement='left'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <div className='flex ml-5 mt-2'>
-              <img src={logo} className='w-28' alt='logo' />
+    <div className='navbar absolute mt-[450px] bg-base-100 h-[10px]'>
+      <button
+        ref={btnRef}
+        display={{ base: "block", md: "none" }}
+        onClick={onOpen}
+        size={"sm"}
+        //   background={"none"}
+      >
+        <MdOutlineDashboard className='text-[#FF3811] w-full' size={38} />
+      </button>
+      <Drawer
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <div className='flex ml-5 mt-2'>
+            <img src={logo} className='w-28' alt='logo' />
+          </div>
+          <hr className='my-10' />
+          <DrawerBody>
+            <div className='flex flex-col font-medium text-2xl space-y-4'>
+              <Link to='/'>Home</Link>
+              <Link to='/about'>About</Link>
+              <Link to='/jobs'>Jobs</Link>
+              <Link to='/contacts'>Contact</Link>
+              <Link to='/signup'>Register</Link>
+              <button className='flex px-5 py-2 items-center space-x-1 font-semibold text-lg bg-[#FF3811] text-white rounded-lg'>
+                <p className=''>Post a Job</p>
+                <MdPostAdd size={30} />
+              </button>
             </div>
-            <hr className='my-10' />
-            <DrawerBody>
-              <div className='flex flex-col font-medium text-2xl space-y-4'>
-                <Link>Home</Link>
-                <Link>About</Link>
-                <Link>Jobs</Link>
-                <Link>Contact</Link>
-                <Link to='/signup'>Register</Link>
-                <button className='flex px-5 py-2 items-center space-x-1 font-semibold text-lg bg-[#FF3811] text-white rounded-lg'>
-                  <p className=''>Post a Job</p>
-                  <MdPostAdd size={30} />
-                </button>
-              </div>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-        <div className='flex-1'>
-          <Link
-            to='/'
-            className='text-3xl md:text-4xl font-bold ml-1 text-[#FF3811]'
-          >
-            HireMaster
-          </Link>
-        </div>
-        <div className='flex-none'>
-          <ul className='hidden md:flex  items-center space-x-4 mr-4 font-medium text-lg menu menu-horizontal px-1'>
-            <Link>Home</Link>
-            <Link>About</Link>
-            <Link>Jobs</Link>
-            <Link>Contact</Link>
-            <Link to='/signup'>Register</Link>
-          </ul>
-        </div>
-        <button className='hidden md:flex px-5 py-2 items-center space-x-1 font-semibold text-lg bg-[#FF3811] text-white rounded-lg'>
-          <p className=''>Post a Job</p>
-          <MdPostAdd size={30} />
-        </button>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+      <div className='flex-1'>
+        <Link
+          to='/'
+          className='text-3xl md:text-4xl font-bold ml-1 text-[#FF3811]'
+        >
+          HireMaster
+        </Link>
       </div>
+      <div className='flex-none'>
+        <div className='hidden md:flex  items-center space-x-4 mr-4 font-medium text-lg menu menu-horizontal px-1'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/jobs'>Jobs</Link>
+          <Link to='/contacts'>Contact</Link>
+          <Link to='/signup'>Register</Link>
+        </div>
+      </div>
+      <button className='hidden md:flex px-5 py-2 items-center space-x-1 font-semibold text-lg bg-[#FF3811] text-white rounded-lg'>
+        <p className=''>Post a Job</p>
+        <MdPostAdd size={30} />
+      </button>
     </div>
   );
 };
