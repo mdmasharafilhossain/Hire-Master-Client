16ad64fe14c33f2d1e50b8fd027c53e002fcc5ed
Thouhid98 <thouhidhussain98@gmail.com>
2024-02-01 18:38:49 +0600

    Applied jobs done

--- src/Comonents/Routes/Routes.jsx
+++ src/Comonents/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import About from "../../Pages/About/About";
 import JobDetails from "../../Pages/JobDetails/JobDetails";
 import Profile from "../../Pages/Profile/Profile";
 import UserProfileForm from "../UserProfileForm/UserProfileForm";
+import Appliedjobs from "../../Pages/Appliedjobs/Appliedjobs";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -39,6 +40,10 @@ const router = createBrowserRouter([
       {
         path: "/jobpost",
         element: <JobPost></JobPost>,
+      },
+      {
+        path: "/appliedjobs",
+        element: <Appliedjobs></Appliedjobs>,
       },
       {
         path: "contacts",
