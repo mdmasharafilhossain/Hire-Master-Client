5a636b4c3fce843de03a8745898559761a85f172
M-Arafat61 <arafjust16@gmail.com>
2024-02-02 17:27:47 +0600

    user login/reg error log with jobs design retouch

--- src/Pages/Jobs/Jobs.jsx
+++ src/Pages/Jobs/Jobs.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import SingleJobList from "../../Comonents/JobList/SingleJobList";
 import UseAxiosPublic from "../../Comonents/Hooks/UseAxiosPublic/UseAxiosPublic";
 import useFetchData from "../../Comonents/Hooks/UseFetchData/useFetchData";
 import JobFilter from "../../Comonents/JobFilter/JobFilter";
 import Loader from "../../Comonents/Loader/Loader";
+import Navbar2 from "../../Comonents/Navbar/Navbar2";
 
 const Jobs = () => {
   const axiosPublic = UseAxiosPublic();
@@ -44,16 +44,27 @@ const Jobs = () => {
   console.log(filterJobs);
   return (
     <div className='mx-auto'>
-      <div className='flex items-center justify-between  my-10 '>
-        <h1 className='text-3xl font-bold'>
-          Find your dream job abroad or remote
-        </h1>
-        <Link to='/'>
-          <button className='btn btn-outline btn-sm mt-2'>Back</button>
-        </Link>
+      <Navbar2 />
+      <div className='mt-20'>
+        <div className='flex gap-x-5 md:gap-x-10 justify-center items-center px-4 py-5 sm:px-6'>
+          <div className='bg-[#FF3811] h-40 w-[6px] rounded-full'></div>
+          <div className='text-center '>
+            <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-3xl md:text-5xl'>
+              <span className='block lg:inline'>Find Your Perfect</span>{" "}
+              <span className='block text-[#FF3811] lg:inline'>
+                Job Opportunity!!
+              </span>
+            </h1>
+            <p className='mt-3 max-w-[550px]  mx-auto text-base text-gray-500 font-medium sm:text-lg md:mt-5 md:max-w-3xl'>
+              With our comprehensive job listings, you'll never miss out on
+              exciting opportunities. Apply today and start your journey with
+              us.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className='lg:flex lg:gap-x-10'>
-        <div className='lg:w-1/4 px-2'>
+      <div className='px-2 lg:flex lg:gap-x-10 my-10'>
+        <div className='lg:w-1/4'>
           <JobFilter
             handleSubmit={handleSubmit}
             filterParams={filterParams}
