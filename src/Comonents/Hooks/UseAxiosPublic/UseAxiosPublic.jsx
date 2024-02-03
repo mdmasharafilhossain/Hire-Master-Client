5865c6e5495b1ff5a6f64f9cbfadff4b226685fb
M-Arafat61 <arafjust16@gmail.com>
2024-02-03 16:37:05 +0600

    updated jobs page also footer rearrange

--- src/Comonents/Hooks/UseAxiosPublic/UseAxiosPublic.jsx
+++ src/Comonents/Hooks/UseAxiosPublic/UseAxiosPublic.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 
+// const AxiosPublic = axios.create({
+//     baseURL:"https://hire-master-server.vercel.app"
+// })
 
 const AxiosPublic = axios.create({
-    baseURL:"https://hire-master-server.vercel.app"
-})
-
-// const AxiosPublic = axios.create({
-//     baseURL:'http://localhost:5000'
-// })
+  baseURL: "http://localhost:5000",
+});
 
 const UseAxiosPublic = () => {
   return AxiosPublic;
--- src/Pages/Jobs/Jobs.jsx
+++ src/Pages/Jobs/Jobs.jsx
@@ -11,6 +11,8 @@ const Jobs = () => {
   const [filterJobs, setFilterJobs] = useState([]);
   const [filterLoading, setFilterLoading] = useState(false);
   const [value, setValue] = useState([0, 250000]);
+  const [filterMessage, setFilterMessage] = useState("");
+
   const [filterParams, setFilterParams] = useState({
     job_title: "",
     job_time: [],
@@ -24,8 +26,8 @@ const Jobs = () => {
       setFilterJobs(jobs);
     }
   }, [jobs]);
+
   if (loading) {
-    // return <p>Loading...</p>;
     return <Loader />;
   }
 
@@ -40,6 +42,7 @@ const Jobs = () => {
       .then(response => {
         setFilterJobs(response.data);
         setFilterLoading(false);
+        setFilterMessage(response.data.message);
         console.log(response.data.message);
       })
       .catch(error => {
@@ -53,24 +56,24 @@ const Jobs = () => {
       <Navbar2 />
       <div className='mt-20'>
         <div className='flex gap-x-5 md:gap-x-10 justify-center items-center px-4 py-5 sm:px-6'>
-          <div className='bg-[#FF3811] h-40 w-[6px] rounded-full'></div>
+          <div className='bg-[#FF3811] h-40 w-[5px] rounded-full'></div>
           <div className='text-center '>
-            <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-3xl md:text-5xl'>
+            <h1 className='text-xl font-bold tracking-tight text-gray-900 md:text-3xl'>
               <span className='block lg:inline'>Find Your Perfect</span>{" "}
               <span className='block text-[#FF3811] lg:inline'>
                 Job Opportunity!!
               </span>
             </h1>
-            <p className='mt-3 max-w-[550px]  mx-auto text-base text-gray-500 font-medium sm:text-lg md:mt-5 md:max-w-3xl'>
-              With our comprehensive job listings, you'll never miss out on
+            <p className='mt-2 max-w-[550px]  mx-auto text-base text-gray-500 font-medium sm:text-lg md:mt-2 md:max-w-3xl'>
+              With our comprehensive job listings, you will never miss out on
               exciting opportunities. Apply today and start your journey with
               us.
             </p>
           </div>
         </div>
       </div>
-      <div className='px-2 lg:flex lg:gap-x-10 my-10'>
-        <div className='lg:w-1/4'>
+      <div className='px-2 md:px-6 container mx-auto lg:flex lg:gap-x-10 my-10'>
+        <div className='lg:w-1/3'>
           <JobFilter
             jobs={jobs}
             handleSubmit={handleSubmit}
@@ -89,8 +92,8 @@ const Jobs = () => {
               <SingleJobList key={filteredJob._id} job={filteredJob} />
             ))
           ) : (
-            <p className='text-center md:text-start text-red-500 text-xl md:text-2xl'>
-              No jobs found matching your criteria.Reset Filters!
+            <p className='text-center md:text-start text-red-500 text-xl md:text-2xl w-3/4 mx-auto'>
+              {filterMessage}
             </p>
           )}
         </div>
