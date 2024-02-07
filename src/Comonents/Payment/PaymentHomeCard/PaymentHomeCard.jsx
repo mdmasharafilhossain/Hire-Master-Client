5e8330f1764fc8a3286b5a814df52e959b69ac15
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-02-07 12:04:43 +0600

    2 card added in home page for subcription

--- src/Comonents/Payment/PaymentHomeCard/PaymentHomeCard.jsx
+++ src/Comonents/Payment/PaymentHomeCard/PaymentHomeCard.jsx
@@ -1,9 +1,79 @@
+import { Link } from "react-router-dom";
 
 
 const PaymentHomeCard = () => {
     return (
-        <div>
-            <h2>Payment</h2>
+        <div className="container mx-auto mt-20">
+             <h2 className='text-center text-5xl font-bold  mb-20'>Our Plans</h2>
+             <div className="grid gap-20  grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:mx-60 ">
+              {/* First Page */}
+               <div className="border-2 shadow-lg border-pink-300 rounded-lg py-5 w-full">
+                <h2 className="mb-10 ml-5 border rounded-md font-bold w-40 px-2 py-1 bg-pink-300">Free For 1 Months</h2>
+                <div className="flex lg:ml-5   gap-40 mb-10">
+                     <div className="font-bold uppercase">
+                        <h2> Premium<br></br>
+                         Individual</h2>
+                     </div>
+                     <div>
+                          <h1 className="font-bold uppercase">Free</h1>
+                          <p>For 1 Months</p> 
+                     </div>
+                </div>
+                <div>
+                    <div className="ml-5 font-bold">
+                        <li><span>1 Premium Account</span></li>
+                        <li>Cancel Anytime</li>
+                        <li>15 hours/Months To Enjoy Our Service</li>
+                    </div>
+                    
+                </div>
+
+                <Link to="/Subscription"><button className="btn ml-12 text-base btn-secondary border-none text-black font-bold bg-pink-300 hover:bg-pink-400 w-3/4 mt-32 rounded-3xl">Try Free For 1 Months</button></Link>
+                <p className="text-[12px]  text-center">Free for 1 month,then $10.99 per month after <br></br>
+                <span className="underline ">Terms Apply</span>
+                </p>
+
+               </div>
+            
+               
+              {/* Third Page */}
+               <div className="border-2 shadow-lg border-blue-300 rounded-lg py-5 w-full">
+                <div className="flex lg:gap-36">
+                <h2 className="  ml-5 text-xl border rounded-md font-bold px-2 py-1 bg-blue-300">Premium</h2>
+                <div >
+                    <p className="font-bold text-xl">$16.99</p>
+                    <p className="text-[12px]">per months</p>
+                </div>
+
+                </div>
+                
+                <div>
+                    <div className="ml-5 mt-16 font-bold">
+                        <li><span>6 device login</span></li>
+                        <li>Cancel Anytime</li>
+                        <li>24 hours/Day To Enjoy Our Service</li>
+                        <li>Apply Unlimited Jobs</li>
+                        <li>See details about HR Profile</li>
+                    </div>
+                    
+                </div>
+
+                <Link to="/Subscription"><button className="btn  ml-12 text-base btn-secondary border-none text-black font-bold bg-blue-300 hover:bg-blue-400 w-3/4 mt-[150px] rounded-3xl">Get Premium</button></Link>
+                <p className="text-[12px] ml-5 text-center">For 6 people who reside at the same address<br></br>
+                <span className="underline ">Terms Apply</span>
+                </p>
+
+               </div>
+              
+                
+
+
+
+
+
+
+
+            </div>
         </div>
     );
 };
