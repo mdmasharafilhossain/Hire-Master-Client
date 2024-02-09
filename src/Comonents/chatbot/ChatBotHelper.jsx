80d627b2bdbc87787c04858f78c8c2e4f474b175
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-09 19:27:31 +0600

    added color in chatbot

--- src/Comonents/chatbot/ChatBotHelper.jsx
+++ src/Comonents/chatbot/ChatBotHelper.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import ChatBot from "react-simple-chatbot";
-// import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
-// const CHATBOT_THEME = {
-//     background: "#FFFEFC",
-//     fontFamily: "Roboto",
-//     headerBgColor: "#ff6445",
-//     headerFontColor: "#fff",
-//     headerFontSize: "15px",
-//     botBubbleColor: "black",
-//     botFontColor: "#fff",
-//     userBubbleColor: "#ff6445",
-//     userFontColor: "#fff",
-//     height: '200px'
-// };
+const CHATBOT_THEME = {
+    background: "#FFFEFC",
+    fontFamily: "Roboto",
+    headerBgColor: "#ff6445",
+    headerFontColor: "#fff",
+    headerFontSize: "15px",
+    botBubbleColor: "black",
+    botFontColor: "#fff",
+    userBubbleColor: "#ff6445",
+    userFontColor: "#fff",
+    height: '200px'
+};
 
 const ChatBotHelper = () => {
     const [steps, setSteps] = useState([
@@ -72,10 +72,10 @@ const ChatBotHelper = () => {
       
     return (
         <>
-            {/* <ThemeProvider theme={CHATBOT_THEME}> */}
+            <ThemeProvider theme={CHATBOT_THEME}>
                 <ChatBot headerTitle="HireMaster"
                     steps={steps} floating={true} />
-            {/* </ThemeProvider> */}
+            </ThemeProvider>
         </>
     );
 };
