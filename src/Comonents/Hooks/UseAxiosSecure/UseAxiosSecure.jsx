7155d19520848fdbcfe3f248985b53da40c20d17
Mohammad Mashrafil Hossain Mahi <mashrafilmahi007@gmail.com>
2024-02-10 15:48:42 +0600

    useAxiosSecure hooks added

---
+++ src/Comonents/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+
+const AxiosSecure = axios.create({
+    baseURL:'http://localhost:5000'
+})
+// const AxiosSecure = axios.create({
+//     baseURL:'https://hire-master-server.vercel.app'
+// })
+
+const UseAxiosSecure = () => {
+    return AxiosSecure;
+};
+
+export default UseAxiosSecure;
