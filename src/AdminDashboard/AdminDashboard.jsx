358c0b1b3c88f9813d40738ca8f49d45e296aae2
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-02-13 12:16:01 +0600

    responsiveness added in admin dasboard

--- src/AdminDashboard/AdminDashboard.jsx
+++ src/AdminDashboard/AdminDashboard.jsx
@@ -18,11 +18,11 @@ const AdminDashboard = () => {
       </div> 
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-        <ul className="menu p-4 w-80 min-h-full bg-orange-600 text-base-content">
+        <ul className="menu p-4 w-60 min-h-full bg-orange-600 text-base-content">
     
           <div className="space-y-2 mb-10">
-            <img className="w-28 ml-20 mt-10 rounded-full" src={user?.photoURL} alt="" />
-            <h1 className="text-lg text-white font-bold ml-5 ">{user?.displayName}</h1>
+            <img className="w-20 ml-16 mt-6 rounded-full" src={user?.photoURL} alt="" />
+            <h1 className="text-sm text-white text-center font-bold">{user?.displayName}</h1>
           </div>
 
 
@@ -30,7 +30,7 @@ const AdminDashboard = () => {
 
 
           <ul className="menu p-4 py-auto"> 
-                    <li><NavLink to="/AdminDashboard/AllUsers">All Users</NavLink></li>
+                    <li  className="font-bold text-sm text-white"><NavLink to="/AdminDashboard/AllUsers" className="text-sm font-bold">All Job Seekers</NavLink></li>
                     
                     
                </ul>
