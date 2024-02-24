65179f5334603b9cde1390844579f6023090f4ca
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-24 23:23:08 +0600

    title short description and cover photo showed in single course details page

--- src/Comonents/PremiumUserCourse/SingleCourse.jsx
+++ src/Comonents/PremiumUserCourse/SingleCourse.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import useCourses from "../Hooks/Courses/UseCourses";
 
 const SingleCourse = () => {
@@ -8,10 +8,24 @@ const SingleCourse = () => {
  console.log(courses)
 const course=courses.find(courseData=>courseData.courseId==id)
 console.log(course)
-
+// const {courseId,courseName,instructor,duration,topics,level,price,photoUrl,description,shortDescription,dailyBreakdown}=course
+console.log(course?.photoUrl)
     return (
         <div>
-      
+<div>
+<div className="hero h-96" style={{backgroundImage: `url(${course?.photoUrl})`}}>
+  <div className="hero-overlay bg-opacity-50"></div>
+  <div className=" text-center text-neutral-content">
+    <div >
+      <h1 className="mb-5 text-5xl font-bold">{course?.courseName}</h1>
+      <p className="mb-5">{course?.shortDescription}</p>
+    </div>
+  </div>
+</div>
+</div>
+<div>
+    
+</div>
         </div>
     );
 };
