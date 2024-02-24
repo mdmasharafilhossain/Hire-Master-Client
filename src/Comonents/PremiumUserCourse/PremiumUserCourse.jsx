c9dbdcc085ad0c7aa6ed2bc66a69cdce65f09436
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-24 14:44:38 +0600

    changes the data in json file

--- src/Comonents/PremiumUserCourse/PremiumUserCourse.jsx
+++ src/Comonents/PremiumUserCourse/PremiumUserCourse.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 
 
 const PremiumUserCourse = () => {
 
+const [course,setCourse]=useState([])
+useEffect(()=>{
+    fetch("../../../public/course.json")
+    .then(res=>res.json())
+    .then(data=>setCourse(data))
+},[])
 
-
+console.log(course)
     return (
         <div>
             
