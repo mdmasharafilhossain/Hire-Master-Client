e0b8d0e40ba1f0e5eee7898d0715395de5dbb6af
Miran Ibrahim <moeenuddin.miran@gmail.com>
2024-02-25 12:35:57 +0600

    password visibility toggling feature

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,55 +1,57 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import swal from "sweetalert";
 import { AuthContext } from "../../Comonents/AuthProvider/AuthProvider";
 import { saveUsersInDb } from "../../api";
 import Navbar2 from "../../Comonents/Navbar/Navbar2";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
   const { signIn, googleSignIn } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
   const [invalidAuth, setInvalidAuth] = React.useState("");
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
 
-  const saveUser = async user => {
+  const saveUser = async (user) => {
     const response = await saveUsersInDb(user);
     console.log(response.data);
   };
   // google sign in
   const handleGoogleSignIn = () => {
     googleSignIn()
-      .then(result => {
+      .then((result) => {
         if (result) {
           const user = {
             name: result.user.displayName,
             email: result.user.email,
-            photo: result?.user?.photoURL
+            photo: result?.user?.photoURL,
           };
           saveUser(user);
         }
         navigate(from, { replace: true });
         return swal("Success!", "Login Successful", "success");
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error.message);
       });
   };
-  const handleLogin = e => {
+  const handleLogin = (e) => {
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
 
     signIn(email, password)
-      .then(result => {
+      .then((result) => {
         if (result) {
           navigate(location?.state ? location.state : "/");
           return swal("Success!", "Login Successful", "success");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         setInvalidAuth(error?.code);
       });
   };
@@ -57,74 +59,82 @@ const Login = () => {
   return (
     <>
       <Navbar2 />
-      <div className='my-20 md:my-32 container mx-auto flex flex-col lg:flex-row space-x-10 justify-between items-center'>
-        <div className='w-full mx-auto'>
+      <div className="my-20 md:my-32 container mx-auto flex flex-col lg:flex-row space-x-10 justify-between items-center">
+        <div className="w-full mx-auto">
           <img
-            className=''
-            src='https://i.ibb.co/VCWMv70/10173919-8619.jpg'
-            alt=''
+            className=""
+            src="https://i.ibb.co/VCWMv70/10173919-8619.jpg"
+            alt=""
           />
         </div>
-        <div className='w-full mx-auto lg:w-3/4 border rounded-2xl overflow-hidden px-10 md:px-16 lg:px-24 py-10 md:py-14 lg:py-20'>
-          <div className='card '>
-            <div className='flex flex-col w-full'>
-              <div className='text-center'>
-                <h1 className='text-2xl md:text-3xl font-semibold mb-5'>
+        <div className="w-full mx-auto lg:w-3/4 border rounded-2xl overflow-hidden px-10 md:px-16 lg:px-24 py-10 md:py-14 lg:py-20">
+          <div className="card ">
+            <div className="flex flex-col w-full">
+              <div className="text-center">
+                <h1 className="text-2xl md:text-3xl font-semibold mb-5">
                   Log in to HireMaster{" "}
                 </h1>
               </div>
               <div>
-                <form className='space-y-4' onSubmit={handleLogin}>
-                  <div className='form-control'>
+                <form className="space-y-4" onSubmit={handleLogin}>
+                  <div className="form-control">
                     <input
-                      type='email'
-                      name='email'
-                      placeholder='Your Email'
-                      className='border border-gray-300 px-3 py-2 outline-none'
+                      type="email"
+                      name="email"
+                      placeholder="Your Email"
+                      className="input input-bordered w-full pr-10"
                       required
                     />
                   </div>
-                  <div className='form-control'>
-                    <input
-                      type='password'
-                      name='password'
-                      placeholder='Password'
-                      className='border border-gray-300 px-3 py-2 outline-none'
-                      required
-                    />
+                  <div className="form-control">
+                    <div className="relative">
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        placeholder="password"
+                        className="input input-bordered w-full pr-10" // Added pr-10 for padding on the right
+                        required
+                      />
+                      <span
+                        className="absolute inset-y-0 right-3 flex items-center cursor-pointer" // Adjusted position to the right
+                        onClick={() => setShowPassword(!showPassword)} // Toggles the show/hide of password
+                      >
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                      </span>
+                    </div>
                   </div>
-                  <p className='text-red-500 px-3'>{invalidAuth}</p>
-                  <div className='form-control'>
+                  <p className="text-red-500 px-3">{invalidAuth}</p>
+                  <div className="form-control">
                     <button
-                      type='submit'
-                      className='group relative overflow-hidden py-2 rounded-2xl bg-[#FF3811] font-semibold text-white'
+                      type="submit"
+                      className="group relative overflow-hidden py-2 rounded-2xl bg-[#FF3811] font-semibold text-white"
                     >
                       Login
-                      <div className='absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30'></div>
+                      <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
                     </button>
                   </div>
-                  <div className='flex justify-center items-center'>
-                    <span className='w-full border border-[#FF3811]'></span>
-                    <span className='px-4'>Or</span>
-                    <span className='w-full border border-[#FF3811]'></span>
+                  <div className="flex justify-center items-center">
+                    <span className="w-full border border-[#FF3811]"></span>
+                    <span className="px-4">Or</span>
+                    <span className="w-full border border-[#FF3811]"></span>
                   </div>
                   <button
                     onClick={handleGoogleSignIn}
-                    className='btn btn-outline btn-warning w-full rounded-md overflow-hidden text-xs sm:text-xl  font-bold'
+                    className="btn btn-outline btn-warning w-full rounded-md overflow-hidden text-xs sm:text-xl  font-bold"
                   >
-                    <FcGoogle className='text-xl' /> Continue with Google
+                    <FcGoogle className="text-xl" /> Continue with Google
                   </button>
                 </form>
-                <div className='form-control mt-16 space-y-5'>
-                  <div className='flex justify-between items-center'>
-                    <span className='w-full border border-[#FF3811]'></span>
-                    <span className='text-xs sm:text-base w-full text-nowrap px-2'>
+                <div className="form-control mt-16 space-y-5">
+                  <div className="flex justify-between items-center">
+                    <span className="w-full border border-[#FF3811]"></span>
+                    <span className="text-xs sm:text-base w-full text-nowrap px-2">
                       Do not have an account?
                     </span>
-                    <span className='w-full border border-[#FF3811]'></span>
+                    <span className="w-full border border-[#FF3811]"></span>
                   </div>
-                  <Link to='/signup' className='w-full mx-auto'>
-                    <button className='flex font-semibold mx-auto justify-center w-full sm:w-1/2 border px-5 py-2 rounded-full border-yellow-300'>
+                  <Link to="/signup" className="w-full mx-auto">
+                    <button className="flex font-semibold mx-auto justify-center w-full sm:w-1/2 border px-5 py-2 rounded-full border-yellow-300">
                       Sign up
                     </button>
                   </Link>
