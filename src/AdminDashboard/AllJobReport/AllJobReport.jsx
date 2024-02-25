1d794ce89babb6cb15ac2041b3ece985fb1f86eb
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-25 14:37:29 +0600

    report details shown by using modal

--- src/AdminDashboard/AllJobReport/AllJobReport.jsx
+++ src/AdminDashboard/AllJobReport/AllJobReport.jsx
@@ -54,7 +54,28 @@ console.log(reports)
                                 <td className='font-bold'> <h1>{report?.user_email}</h1></td>
                                 <td className='font-bold'> <h1>{report?.hiring_manager_email}</h1></td>
                                 <td className='font-bold'> <h1>{report?.job_title}</h1></td>
-                                <td className='font-bold'> <h1>{report?.user_name}</h1></td>
+
+                          {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+
+                                <td > <button className="btn btn-sm text-[#FF3811]" onClick={()=>document.getElementById('my_modal_1').showModal()}>View Details</button>
+<dialog id="my_modal_1" className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Title: {report.job_title}</h3>
+    <p className="py-4 font-medium">Hiring Manager Email: {report?.hiring_manager_email} </p>
+    <p className="py-4"><span className="font-semibold text-red-500">Report:</span> {report?. user_comments} </p>
+    <p className="py-4"><span className="font-medium"> User Name:</span> {report?.user_name} </p>
+    <p className="py-4"><span className="font-medium">User Email:</span>  {report?.user_email} </p>
+
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+</td>
                                  </tr>)
                         }
                         
