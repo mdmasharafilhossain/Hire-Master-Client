19a73a03e1a2deb8bc2f4444f546883652662988
Mohosin212601~ <mohosin2126@gmail.com>
2024-02-25 12:46:51 +0600

    getting  users report by using react hook form

--- src/Pages/JobPostReport/JobPostReport.jsx
+++ src/Pages/JobPostReport/JobPostReport.jsx
@@ -4,14 +4,18 @@ import Loader from "../../Comonents/Loader/Loader";
 import { AuthContext } from "../../Comonents/AuthProvider/AuthProvider";
 import { useContext } from "react";
 import UseAxiosPublic from "../../Comonents/Hooks/UseAxiosPublic/UseAxiosPublic";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 
 const JobPostReport = () => {
 
     const {id}=useParams()
     const { user } = useContext(AuthContext);
+   console.log(user)
   const AxiosPublic = UseAxiosPublic();
-    console.log(id)
+  const { register, handleSubmit, reset } = useForm();
+  
     const { data: job, loading, refetch } = useFetchData(
         `/staticjobpost/${id}`,
         "job"
@@ -22,28 +26,135 @@ const JobPostReport = () => {
       }
     
       refetch();
-      console.log(job);
+      
       const {
         job_title,
         company_name,
-        job_role,
-        salary,
-        job_time,
-        skills,
-        job_description,
-        hiring_manager_name,
-        hiring_manager_photo,
         hiring_manager_email,
-        responsibilities,
-        benefits,
-        qualification,
-        job_location,
       } = job;
     
+  
+  const onSubmit = async (data) => {
+    const formData = {
+      job_title: data.name,
+      company_name: data.companyname,
+      hiring_manager_email: data.manageremail,
+      user_email: data.useremail,
+      user_name:data.username,
+      user_comments:data.comments
+     
+    };
+    console.log(formData);
+
+  };
 
     return (
         <div>
-            
+            <form className="p-10" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full my-6">
+          <label className="label">
+            <span className="label-text font-serif font-bold text-lg ">
+              Job Title*
+            </span>
+          </label>
+          <input
+            type="text"
+            placeholder="Job Title"
+            {...register("name", { required: true })}
+            required
+            className="input input-bordered w-full"
+            defaultValue={ job_title}
+            readOnly
+          />
+        </div>
+        <div className="flex gap-6">
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text font-serif font-bold text-lg ">
+                Company Name*
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Company Name"
+              {...register("companyname", { required: true })}
+              required
+              className="input input-bordered w-full"
+              defaultValue={company_name}
+              readOnly
+            />
+          </div>
+
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text font-serif font-bold text-lg ">
+                Hiring Manager Email*
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Hiring Manager Email"
+              {...register("logo", { required: true })}
+              className="input input-bordered w-full"
+              defaultValue={hiring_manager_email}
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="flex gap-6">
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text font-serif font-bold text-lg ">
+                User Name*
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="User Name"
+              {...register("username", { required: true })}
+              required
+              className="input input-bordered w-full"
+              
+            />
+          </div>
+
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text font-serif font-bold text-lg ">
+               user Email*
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="User Email"
+              {...register("useremail", { required: true })}
+              className="input input-bordered w-full"
+              defaultValue={user?.email}
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="form-control w-full my-6">
+          <label className="label">
+            <span className="label-text font-serif font-bold text-lg ">
+              Your Comments*
+            </span>
+          </label>
+          <input
+            type="text"
+            placeholder="Your Comments"
+            {...register("comments", { required: true })}
+            required
+            className="input input-bordered w-full"
+          />
+        </div>
+        <button
+          className="btn btn-warning w-full bg-white text-black text-xl font-semibold hover:bg-orange-500 hover:text-white"
+          // className="btn w-full bg-orange-600 text-white"
+        >
+         Submit
+        </button>
+      </form>
         </div>
     );
 };
