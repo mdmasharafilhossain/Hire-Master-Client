e5aa550e594b88a1e59f43cb318730b338f32ae0
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-02-28 23:50:29 +0600

    bug fixed

--- src/Comonents/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
+++ src/Comonents/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 
 
 const AxiosSecure = axios.create({
@@ -9,14 +8,6 @@ const AxiosSecure = axios.create({
 // const AxiosSecure = axios.create({
 //     baseURL:'https://hire-master-server.vercel.app'
 // })
-=======
-const AxiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
-});
-// const AxiosSecure = axios.create({
-//   baseURL: "https://hire-master-server.vercel.app",
-// });
->>>>>>> a26aa07cf62edcf3c7ff91395b991dbff05802af
 
 const UseAxiosSecure = () => {
   return AxiosSecure;
