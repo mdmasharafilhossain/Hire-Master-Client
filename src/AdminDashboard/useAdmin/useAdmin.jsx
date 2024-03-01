cf38cdb29e10e53f4681ebf3ca676387b82f4578
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-03-01 10:40:13 +0600

    verify admin functionality added succesfully

--- src/AdminDashboard/useAdmin/useAdmin.jsx
+++ src/AdminDashboard/useAdmin/useAdmin.jsx
@@ -1,11 +1,26 @@
 
+import { useContext } from 'react';
+import { AuthContext } from '../../Comonents/AuthProvider/AuthProvider';
+import UseAxiosSecure from './../../Comonents/Hooks/UseAxiosSecure/UseAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const useAdmin = () => {
-    return (
-        <div>
+    const {user,loading} = useContext(AuthContext);
+    const axiosSecure = UseAxiosSecure();
+    // console.log(user.email)
+    const {data:isUserAdmin} = useQuery({
+        queryKey:[user?.email, 'isUserAdmin'],
+        enabled:!loading && !!user?.email,
+        queryFn: async()=>{
+            if(user?.email){
+                const res = await axiosSecure.get(`/users/checkAdmin/${user?.email}`);
+            console.log(res.data);
+            return res.data?.admin;
+            }
             
-        </div>
-    );
+        }
+    })
+    return [isUserAdmin]
 };
 
 export default useAdmin;
