1e9cefd6dd58f7780b7c823ef5d5d027010ea658
Mohosin212601~ <mohosin2126@gmail.com>
2024-03-04 21:26:44 +0600

    uncomment base url

--- src/Comonents/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
+++ src/Comonents/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const AxiosSecure = axios.create({
-    baseURL:'http://localhost:5000'
-})
 // const AxiosSecure = axios.create({
-//     baseURL:'https://hire-master-server.vercel.app'
+//     baseURL:'http://localhost:5000'
 // })
+const AxiosSecure = axios.create({
+    baseURL:'https://hire-master-server.vercel.app'
+})
 
 const UseAxiosSecure = () => {
   return AxiosSecure;
