097b1be9b65f96475ffc092af8763d82c6226b47
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-03-06 23:23:17 +0600

    Data loaded successfully

--- src/AdminDashboard/UserDetails/UseUserDetails/UseUserDEtails.jsx
+++ src/AdminDashboard/UserDetails/UseUserDetails/UseUserDEtails.jsx
@@ -1,14 +1,16 @@
 import { useContext } from "react";
 import UseAxiosSecure from "../../../Comonents/Hooks/UseAxiosSecure/UseAxiosSecure";
 import { AuthContext } from "../../../Comonents/AuthProvider/AuthProvider";
-import { useQuery } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+
 
 
 const UseUserDEtails = () => {
     const axiosSecure = UseAxiosSecure();
     const {loading} = useContext(AuthContext);
-    const {data:Details=[]}=useQuery({
+    const { data: Details= [],isFetching}=useQuery({
         queryKey:["Details"],
+        
         queryFn:async()=>{
             const res =await axiosSecure.get("/userProfile/all")
             return res.data
--- src/AdminDashboard/UserDetails/UserDetails.jsx
+++ src/AdminDashboard/UserDetails/UserDetails.jsx
@@ -1,17 +1,28 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import UseUserDEtails from "./UseUserDetails/UseUserDEtails";
+import useFetchData from "../../Comonents/Hooks/UseFetchData/useFetchData";
+import Loader from "../../Comonents/Loader/Loader";
+
 
 
 const UserDetails = () => {
-    const UsersInfo = useLoaderData();
-    console.log(UsersInfo);
-    const { email } = useParams();
-    console.log("Database email",email)
-    // const UserCard = UsersInfo.find(info =>info?.email == email);
-    const UserCard = UsersInfo.find(card => card.email == email)
-    console.log(UserCard);
+    const {email} = useParams();
+    console.log(email)
+    const { data: profile, loading, refetch } = useFetchData(
+        `/userProfile/all`,
+        "profile"
+      );
+      if (loading) return <Loader />;
+    
+      refetch();
+    console.log(profile);
+    // const [Details] = UseUserDEtails();
+    // console.log(Details);
+    const UserDetail = profile?.find(Info =>Info?.email === email );
+    console.log(UserDetail)
     return (
         <div>
-            {/* <h2>{UserCard.length}</h2> */}
+            <h2>{UserDetail.email}</h2>
         </div>
     );
 };
