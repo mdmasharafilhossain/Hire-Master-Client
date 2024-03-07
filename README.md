565775a896febccf4255e60060b075b6f7b91ace
abu-rayhan66 <mraburayhan66@gmail.com>
2024-03-07 12:08:36 +0600

    added README.md and fix some issue

--- README.md
+++ README.md
@@ -3,3 +3,51 @@
 
 
 
+
+![screenshot](https://camo.githubusercontent.com/7de37139d0b4c1ce40865e799b446c0e963a3dd8fb68d239707237c40604fa3d/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966.gif)
+
+## Key Features
+
+* LivePreview - Make changes, See changes
+  - Instantly see what your Markdown documents look like in HTML as you create them.
+* Sync Scrolling
+  - While you type, LivePreview will automatically scroll to the current location you're editing.
+* GitHub Flavored Markdown  
+* Syntax highlighting
+* [KaTeX](https://khan.github.io/KaTeX/) Support
+* Dark/Light mode
+* Toolbar for basic Markdown formatting
+* Supports multiple cursors
+* Save the Markdown preview as PDF
+* Emoji support in preview :tada:
+* App will keep alive in tray for quick usage
+* Full screen mode
+  - Write distraction free.
+* Cross platform
+  - Windows, macOS and Linux ready.
+
+## How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/mdmasharafilhossain/HireMaster-Client.git
+
+# Go into the repository
+$ cd electron-markdownify
+
+# Install dependencies
+$ npm install
+
+# Run the app
+$ npm run dev
+```
+
+
+
+
+
+
+
+
--- src/Comonents/Payment/PaymentHomeCard/PaymentHomeCard.jsx
+++ src/Comonents/Payment/PaymentHomeCard/PaymentHomeCard.jsx
@@ -20,8 +20,8 @@ const PaymentHomeCard = () => {
              <h2 className='text-center text-5xl font-bold  mb-20'>Our Plans</h2>
              <div className="grid gap-20  grid-cols-1 md:grid-cols-2 lg:grid-cols-2  mx-5   md:mx-5 lg:mx-60 ">
               {/* First Card */}
-               <div className="border-2 shadow-xl border-yellow-200 rounded-lg py-5 w-full">
-                <h2 className="mb-10 ml-5 border rounded-md font-bold w-40 px-2 py-1 bg-yellow-200 text-black">Free For 1 Months</h2>
+               <div className="border-2 shadow-xl border-yellow-200 rounded-lg py-5 w-full overflow-y-scroll">
+                <h2 className="mb-10 ml-5 border rounded-md font-bold w-40 px-2 py-1 bg-yellow-200 text-black ">Free For 1 Months</h2>
                 <div className="flex ml-5 md:ml-5 lg:ml-5 gap-32 md:gap-24 lg:gap-40 mb-10">
                      <div className="font-bold uppercase">
                         <h2> Premium<br></br>
@@ -50,7 +50,7 @@ const PaymentHomeCard = () => {
             
                
               {/* Second Card */}
-               <div className="border-2 shadow-lg border-orange-300 rounded-lg py-5 w-full">
+               <div className="border-2 shadow-lg border-orange-300 rounded-lg py-5 w-full overflow-y-scroll">
                 <div className="flex gap-32 md:gap-28 lg:gap-36">
                 <h2 className="  ml-5 text-xl border rounded-md font-bold px-2 py-1 bg-orange-300 h-10 text-black">Premium</h2>
                 <div >
